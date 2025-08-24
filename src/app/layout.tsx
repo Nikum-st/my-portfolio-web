38f23ba6d00807060d0775c76a5c7b28245697c3
Никита Уманский <nikum_st@MacBookPro.station>
2025-08-24 10:25:48 +0200

    create layout.tsx

---
+++ src/app/layout.tsx
@@ -0,0 +1,42 @@
+import { Metadata } from "next";
+import { Inter } from "next/font/google";
+
+const inter = Inter({ subsets: ["latin"] });
+
+export const metadata: Metadata = {
+  title: "Nikita Umanskii",
+  description: "Portfolio",
+  keywords: [
+    "Nikita Umanskii",
+    "Nikita Umanskiy",
+    "Никита Уманский",
+    "Nikita Umanskii - developer",
+    "Никита Уманский - разработчик",
+    "Никита Уманский - айтишник",
+    "Portfolio",
+    "Frontend Developer",
+    "Portfopio-React",
+    "Next.js",
+  ],
+  authors: [
+    { name: "Nikita Umanskii", url: "https://nikitaumanskii.vercel.app" },
+  ],
+  openGraph: {
+    title: "Nikita Umanskii",
+    description: "Portfolio",
+    url: "https://nikitaumanskii.vercel.app",
+    siteName: "Nikita Umanskii",
+  },
+};
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <html lang="en">
+      <body className={inter.className}>{children}</body>
+    </html>
+  );
+}
